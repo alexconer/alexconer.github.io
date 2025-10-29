01dfc2e29c05d2b083ee53e4414c9f1e22c3cf02
alexconer <shishkin.com@yandex.ru>
2025-10-29 23:55:59 +0300

    feat: homework1

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -42,15 +42,104 @@
  * - category (Категория)
  * - type ('Profit')
  * */
+type Category = {
+    id: string,
+    name: string,
+    photo?: string,
+}
+
+type Product = {
+    id: string,
+    name: string,
+    photo: string,
+    desc?: string,
+    createdAt: string,
+    oldPrice?: number,
+    price: number,
+    category: Category,
+}
+
+type Cost = {
+    id: string,
+    name: string,
+    desc?: string,
+    createdAt: string,
+    amount: number,
+    category: Category,
+    type: 'Cost',
+}
+
+type Profit = {
+    id: string,
+    name: string,
+    desc?: string,
+    createdAt: string,
+    amount: number,
+    category: Category,
+    type: 'Profit',
+}
+
+type Operation = Cost | Profit;
 
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+    const pId = new Date().getTime().toString(36);
+    const cId = new Date().getTime().toString(10);
+    return {
+        id: pId,
+        name: `Product_${pId}`,
+        desc: `Desc_${pId}`,
+        photo: `img\\${pId}.png`,
+        createdAt: createdAt,
+        oldPrice: 0,
+        price: 0,
+        category: {
+            id: cId,
+            name: `Category_${cId}`,
+            photo: `img\\${cId}.png`,
+        }
+    }
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+    const pId = new Date().getTime().toString(36);
+    const cId = new Date().getTime().toString(10);
+
+    if (Math.random() > 0.5) {
+        return {
+            id: pId,
+            name: `Cost_${pId}`,
+            desc: `Desc_${pId}`,
+            createdAt: createdAt,
+            amount: 0,
+            category: {
+                id: cId,
+                name: `Category_${cId}`,
+                photo: `img\\${cId}.png`,
+            },
+            type: 'Cost',
+
+        };
+    } else {
+        return {
+            id: pId,
+            name: `Profit_${pId}`,
+            desc: `Desc_${pId}`,
+            createdAt: createdAt,
+            amount: 0,
+            category: {
+                id: cId,
+                name: `Category_${cId}`,
+                photo: `img\\${cId}.png`,
+            },
+            type: 'Profit',
+        };
+    }
+};
